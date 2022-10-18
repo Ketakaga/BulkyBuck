072e5ff1c2d33c85c9426f98927d24339694eca7
nico <nicolorossi89@gmail.com>
2022-10-18 20:07:34 +0100

    initial commit

---
+++ BulkyBuckWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyBuckWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
